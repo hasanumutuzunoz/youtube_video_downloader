69089822f1ca5a9c1373c7a5e1adb2098351bced
Hasan <hasanumutuzunoz@gmail.com>
2022-10-16 18:35:11 -0700

    Download High Resolution Video Support Added
    Compile audio and video (for >720p)
    To download more than 720p videos from youtube, we need to download separate audio and video files, then compile them into one video file. 
    I added ffmpeg library for this purpose. Now I can download any resolution video including 8K 4320p. 
    Program downloads audio and video files
    Compiles into one video file into windows downloads folder

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import io
+import shutil
 import tkinter as tk
 from tkinter import *
 from tkinter import ttk
@@ -8,43 +9,53 @@ from PIL import Image, ImageTk
 import urllib.request
 from io import BytesIO
 from urllib.request import urlopen
+import ffmpeg
+import re
+import os
 
 """
 THINGS TO DO
-* Combine audio and video to download higher resolutions 
+
+* Progress bar for compiling audio and video
 * Erase downloaded label when paste link, click download
-* Get 240p and 480p, they are missing
+* Get and paste video Author, Published date, number of views, length
 * Create threads to run simultaneously to fix freeze problem when paste link and getting data
 * Create grids instead of paste for all objects
 """
 
 # Global Variables
 percentage_of_completion = 0
-is_pasted = False
 is_selected = False
-get_video = None
+get_video = False
 
-
+"""
 def paste_link_thread():
     root.bind_all("<<Paste>>", paste_link)
 
 
 def fetch_data_pb():
-    """
+    
     while True:
         print("Test")
         if not fetch_data:
             break
-    """
-    """
+  
+    
     fetch_pb = ttk.Progressbar(root, orient='horizontal', mode='indeterminate', length=280)
     fetch_pb.place(x=200, y=165)
     fetch_pb.start()
-    """
+    
+"""
 
 
 # PASTE LINK function - get streams
 def paste_link(a):
+    desktop = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Videos')
+    print("The Desktop path is: " + desktop)
+    desktop = desktop + "\last_video.mp4"
+    path = re.sub(r'\\', '/', desktop)
+    print(path)
+
     # Capture the link (url) and locate it from YouTube
     yt = YouTube(str(link.get()))
 
@@ -58,20 +69,23 @@ def paste_link(a):
     u = urlopen(yt.thumbnail_url)
     raw_data = u.read()
     u.close()
-    image = Image.open(io.BytesIO(raw_data)).resize((320,240))  # Resize the url image
-    image = ImageTk.PhotoImage(image)                           # Convert the image to ImageTk
-    thumbnail.image = image # Save reference to image
+    image = Image.open(io.BytesIO(raw_data)).resize((320, 240))  # Resize the url image
+    image = ImageTk.PhotoImage(image)  # Convert the image to ImageTk
+    thumbnail.image = image  # Save reference to image
     thumbnail.config(image=image)
 
-    # Place select menu
+    # Select menu
     def get_stream(is_video):
         global get_video
-        if is_video: # Set Video menu
-            streams = [stream.resolution for stream in yt.streams.filter(progressive=True)]
+        if is_video:  # Set Video menu
+            streams = [stream.resolution for stream in
+                       yt.streams.filter(adaptive=True, file_extension='mp4', type='video')]
+            # streams = [stream for stream in yt.streams.filter(adaptive=True, file_extension='mp4', type='video')]
+            print(streams)
             variable.set("Select Video")
             get_video = True
 
-        else: # Set Audio menu
+        else:  # Set Audio menu
             streams = [stream.abr for stream in yt.streams.filter(only_audio=True).order_by('abr')]
             variable.set("Select Audio")
             get_video = False
@@ -79,16 +93,18 @@ def paste_link(a):
         menu = OptionMenu(root, variable, *streams)
         menu.place(x=500, y=330)
 
-    if not globals()["is_pasted"]:
-        # Get video and audio buttons that brings options menu
-        Button(root, text='Get Video', font='san-serif 16 bold', bg='grey', padx=2, command=lambda: get_stream(True)).place(x=400, y=250)
-        Button(root, text='Get Audio', font='san-serif 16 bold', bg='grey', padx=2, command=lambda: get_stream(False)).place(x=600, y=250)
-        globals()["is_pasted"] = True
+    # Get video and audio buttons that brings options menu
+    Button(root, text='Get Video', font='san-serif 16 bold', bg='grey', padx=2, command=lambda: get_stream(True)).place(
+        x=400, y=250)
+    Button(root, text='Get Audio', font='san-serif 16 bold', bg='grey', padx=2,
+           command=lambda: get_stream(False)).place(x=600, y=250)
+
+    root.update()
 
 
 # MENU ON CLICK
 def callback(*args):
-    #print(f"the variable has changed to '{variable.get()}'")
+    # print(f"the variable has changed to '{variable.get()}'")
     if not globals()["is_selected"]:
         # Percentage Label
         value_label.place(x=480, y=400)
@@ -99,34 +115,56 @@ def callback(*args):
         globals()["is_selected"] = True
 
 
-# Download Button function
+# DOWNLOAD BUTTON
 def download():
-    # Capture the link (url) and locate it from YouTube
-    yt = YouTube(str(link.get()))
-
-    # On Progress Function
-    yt.register_on_progress_callback(on_progress)
+    yt = YouTube(str(link.get()))                               # Capture the link (url) and locate it from YouTube
+    yt.register_on_progress_callback(on_progress)               # On Progress Function
 
     # Get video or audio
-    if get_video:
-        stream = yt.streams.filter(res=variable.get()).first()
-    else:
-        stream = yt.streams.filter(abr=variable.get()).first()
+    if get_video:                                               # Video
+        stream = yt.streams.filter(res=variable.get()).first()  # Filter selected resolution from menu variable
+        title = yt.streams.first().title                        # Get video title
+        res = int(re.sub(r'p', '', variable.get()))             # Remove "p" and convert to int (e.g. 1080p to 1080)
+        #stream.download()                                       # Download video stream
 
-    stream.download()
+        # If video resolution is bigger than 720p
+        # Combine video and audio files into one file
+        # Save it into C:/Users/"USERNAME"/Videos/"VIDEO TITLE".mp4
+        #if res > 720:
+        stream.download(filename="video.mp4")                                            # download video stream
+        video = ffmpeg.input("video.mp4")                                                # video file input
+
+        audio_stream = yt.streams.filter(only_audio=True).first()                        # get audio stream
+        audio_stream.download(filename="audio.mp4")                                      # download audio stream
+        audio = ffmpeg.input("audio.mp4")                                                # audio stream input
+
+        downloads_folder = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Downloads')  # get videos folder path
+        file = downloads_folder + "\\" + title + ".mp4"                                     # video file path in videos
+        path = re.sub(r'\\', '/', file)                                                  # change \ to / in path
+
+        # COMBINE AUDIO AND VIDEO
+        ffmpeg.output(audio, video, path).run()
+        # ffmpeg.output(audio, video, 'C:/Users/hasan/PycharmProjects/youtubeVideoDownloader/last_video.mp4').run()
+
+
+    else:  # Audio
+        stream = yt.streams.filter(abr=variable.get()).first()
+        stream.download()
+
+    # stream.download()    # Download video stream
     Label(root, text="Downloaded", font="ariel 15", bg="green").place(x=200, y=250)
 
-        #.filter(progressive=True, file_extension='mp4')
-        #.order_by('resolution')\
-        #.desc()\
-        #.first()
+    # .filter(progressive=True, file_extension='mp4')
+    # .order_by('resolution')\
+    # .desc()\
+    # .first()
+
 
 # On Progress Function
 def on_progress(stream, chunk, bytes_remaining):
     total_size = stream.filesize
     bytes_downloaded = total_size - bytes_remaining
     globals()["percentage_of_completion"] = bytes_downloaded / total_size * 100
-    print(percentage_of_completion)
     pb['value'] = percentage_of_completion
 
     # Update current progress percentage
@@ -136,8 +174,8 @@ def on_progress(stream, chunk, bytes_remaining):
 
 # Root windows
 root = Tk()
-root.geometry('800x600') # Size of the window
-root.resizable(0,0) # Makes the windows adjustable with its features
+root.geometry('800x600')  # Size of the window
+root.resizable(0, 0)  # Makes the windows adjustable with its features
 root.title('YouTube Downloader')
 Label(root, text='Download YouTube videos for free', font='san-serif 14').place(x=250, y=30)
 link = StringVar()
